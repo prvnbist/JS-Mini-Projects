6ce0407f24f066d34850c94a92a64e033b8bd3f8
Praveen Bisht <prvnbist@gmail>
2017-09-21 16:50:15 +0530

    Added Strength Checker + UI Changes
    1. Password Strength Checker
    2. UI Changes
    3. Responsiveness
    4. Added Script

---
+++ 1. Password Generator & Strength Checker/magic.js
@@ -0,0 +1,48 @@
+function generatePassword() {
+    var length = 8,
+        charPool = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()[]{}<>?/\~",
+        pass = "";
+    for (var i = 0, n = charPool.length; i < length; ++i) {
+        pass += charPool.charAt(Math.floor(Math.random() * n));
+    }
+    return pass;
+}
+
+$(".passGen").on('click',function(){
+	$("#sup").text(generatePassword());
+});
+
+
+$( "input" ).keyup(function() {
+    var pw = $(this).val(),
+    strength = 0;
+    if (pw.match(/[a-zA-Z0-9][a-zA-Z0-9]+/)) {
+        strength += 1;
+    }
+    if (pw.match(/[~<>?]+/)) {
+        strength += 1;
+    }
+    if (pw.match(/[!@#$%^&*()]+/)) {
+        strength += 1;
+    }
+    if(pw.length > 5) {
+        strength += 1;
+    }
+    switch(strength) {
+        case 0:
+                $('.strength').text("Strength");
+                break;
+        case 1:
+                $('.strength').text("Weak");
+                break;
+        case 2:
+                $('.strength').text("Medium");
+                break;
+        case 3:
+                $('.strength').text("Mild");
+                break;
+        case 4:
+                $('.strength').text("Strong");
+                break;
+    }
+});
---
+++ README.md
@@ -0,0 +1,8 @@
+# JS - Mini Projects
+
+This repository holds my JS made mini projects.
+
+1. Password Generator + Strength Checker
+- It generates random string of length 8 from a character pool of  alphanumerics + symbols. As for the strength checker, it checks if the password has Lowecase letters ( a-z ), Uppercase Letters ( A-Z ), Numbers ( 0-9 ) and Symbols.
+- Codepen - https://codepen.io/prvnbist/pen/xXGYdp
+
