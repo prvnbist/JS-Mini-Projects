a5786e9944524f8afacb2c29fbc6361ddc09f390
Praveen Bisht <prvnbist@gmail>
2017-10-28 21:28:30 +0530

    UI & Functionality Changes
    1. Icons & design change.
    2. Add task by pressing enter.

--- 8. Todo App/magic.js
+++ 8. Todo App/magic.js
@@ -1,28 +1,32 @@
 $(function () {
-
-	var btn = $('button'),
-		list = $('ul');
-
-	btn.on('click', addTask);
+	var list = $("ul"),
+		input = $("input");
 
 	//Adding Tasks
-	function addTask() {
-		var input = $('input').val();
-		list.prepend("<li>" +
-			input +
-			'<i class="delete material-icons">close</i>' +
-			'<i class="edit material-icons">mode_edit</i>' +
-			"</li>");
-	}
+	input.keydown(function (e) {
+		if (e.keyCode === 13) {
+			var task = $(this).val();
+			list.prepend(
+				"<li>" +
+				task +
+				'<i class="delete material-icons">delete</i>' +
+				'<i class="edit material-icons">mode_edit</i>' +
+				"</li>"
+			);
+		}
+	});
 
 	//Editing Tasks
-	list.on('click', '.edit', function () {
-		$(this).parent().attr('contenteditable', 'true');
+	list.on("click", ".edit", function () {
+		$(this)
+			.parent()
+			.attr("contenteditable", "true");
 	});
 
 	// Deleting Tasks
-	list.on('click', '.delete', function () {
-		$(this).parent().remove();
+	list.on("click", ".delete", function () {
+		$(this)
+			.parent()
+			.remove();
 	});
-
 });
